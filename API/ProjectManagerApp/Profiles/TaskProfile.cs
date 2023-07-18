e3969534ae35f4850ca4b875a9153f4d8c66117d
José Matos <83038703+josemnmatos@users.noreply.github.com>
2023-07-18 15:43:24 +0100

    Some more endpoints, folder structure change

---
+++ API/ProjectManagerApp/Profiles/TaskProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace ProjectManagerApp.Profiles
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile() 
+        {
+            CreateMap<Entities.Task, Models.TaskDto>();
+            CreateMap<Models.TaskForCreationDto, Entities.Task>();
+            CreateMap<Models.TaskForUpdateDto, Entities.Task>();
+            CreateMap<Entities.Task,Models.TaskForUpdateDto>();
+        }
+
+
+        
+    }
+}
